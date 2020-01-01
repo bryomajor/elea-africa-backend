0cbe103b017c44497ec1a89e3658a2c7899f1b18
Brian Otieno <bryomajor@gmail.com>
2020-01-01 14:11:08 +0300

    register models in the admin.

--- elea/api/admin.py
+++ elea/api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Event, Contact, EventImage
 
 # Register your models here.
+class EventImageInline(admin.TabularInline):
+    model = EventImage
+    extra = 3
+
+class EventAdmin(admin.ModelAdmin):
+    inlines = [ EventImageInline, ]
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Contact)
