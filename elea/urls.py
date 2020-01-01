291a657e9f4e6fc7d3a7f369c48fdc6a62f803d1
Brian Otieno <bryomajor@gmail.com>
2020-01-01 14:10:47 +0300

    set up urls for the api endpoints

--- elea/urls.py
+++ elea/urls.py
@@ -14,8 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import include, url
+from rest_framework import routers
+from elea.api import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+router = routers.DefaultRouter()
+router.register(r'events', views.EventViewSet),
+router.register(r'contact', views.ContactViewSet),
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'admin/', admin.site.urls),
+    url(r'^', include(router.urls)),
+    url(r'api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
