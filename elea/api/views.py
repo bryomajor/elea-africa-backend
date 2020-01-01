dded69b036bd56caca502124efd25fdede897070
Brian Otieno <bryomajor@gmail.com>
2020-01-01 14:16:08 +0300

    set up viewsets for the event and contact serializers.

--- elea/api/views.py
+++ elea/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import Event, Contact
+from rest_framework import viewsets
+from .serializers import EventSerializer, ContactSerializer
 
 # Create your views here.
+
+class EventViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows events to be viewed or edited.
+    """
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+class ContactViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows contacts to be viewed or edited.
+    """
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
